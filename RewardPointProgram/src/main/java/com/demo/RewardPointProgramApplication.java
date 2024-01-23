4207f232a0857ce2193bef5de0907a1886fc9c92
Niki-Panchal <96787531+Niki-Panchal@users.noreply.github.com>
2024-01-22 21:23:29 -0500

    Update RewardPointProgramApplication.java

--- RewardPointProgram/src/main/java/com/demo/RewardPointProgramApplication.java
+++ RewardPointProgram/src/main/java/com/demo/RewardPointProgramApplication.java
@@ -8,6 +8,7 @@ public class RewardPointProgramApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RewardPointProgramApplication.class, args);
+		System.out.println("Test RewardPointProgramApplication");
 	}
 
 }
